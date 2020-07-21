4942873a7ca1c9e85bd392d2b52e8723a98d999c
Wesley Rou <wesleyrou@gmail.com>
2020-07-21 11:13:43 -0700

    fixed delete

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -22,12 +22,11 @@ export default class Note extends React.Component {
         'content-type': 'application/json'
       },
     })
-      .then(res => {
-        console.log(res)
-        if (!res.ok)
-          return res.json().then(e => Promise.reject(e))
-        return res.json()
-      })
+      // .then(res => {
+      //   if (!res.ok)
+      //     return res.json().then(e => Promise.reject(e))
+      //   return res.json()
+      // })
       .then(() => {
         this.context.deleteNote(noteId)
         // allow parent to perform extra behaviour
